a2d105cb3db1445c2d000b263006f2911b232151
unknown <jjoao.monteiro15@Gmail.com>
2022-01-03 15:53:25 -0300

    Finalizando rotas novamente

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,7 @@ import HeroSection from '../HeroSection'
 function Home() {
     return (
         <>
-            <HeroSection src="./images/k-mitch-hodge-dIxNPbPqTuc-unsplash.jpg" alt="imaga-banner" title="Cristosofia" subtitle="Filosofía sem compromisso com ideologia!"/>
+            <HeroSection src="./images/k-mitch-hodge-dIxNPbPqTuc-unsplash.jpg" alt="image-banner" title="Cristosofia" subtitle="Filosofía sem compromisso com ideologia!"/>
             <Article path="/Cristosofia/legado" title="O legado roubado" src="./images/istockphoto-1322882224-170667a.jpg" alt="image" desc="8 textos de explicando como os gregos roubaram a  filosofia egípicia" />
         </>
     )
